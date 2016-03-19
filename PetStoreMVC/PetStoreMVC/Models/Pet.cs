efe2e3792d944b89587f9d9b489f691475cbc41c
toddlasley <todd.r.lasley@gmail.com>
2016-03-19 12:21:08 -0400

    added pet model with properties

---
+++ PetStoreMVC/PetStoreMVC/Models/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetStoreMVC.Models
+{
+    public class Pet
+    {
+        public Int32 ID { get; set; }
+        public string Description { get; set; }
+        public DateTime DateReceived { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
